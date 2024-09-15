01e026e81cf2ebfc55419441a382d9b0394efc20
Milan Ranpariya <ranpariya04@gmail.com>
2024-09-15 19:28:11 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,12 @@
+// Coding Challenge 4 - Inventory System Management
+// Task 1 - Create an inventory array of product objects
+let inventory = [
+    {name: 'Laptop', price: 1000, quantity: 10, lowStockLevel: 2 },
+    {name: 'Smartphone', price: 750, quantity: 20, lowStockLevel: 5 },
+    {name: 'Tablet', price: 600, quantity: 15, lowStockLevel: 4 },
+    {name: 'Smartwatch', price: 150, quantity: 10, lowStockLevel: 2 },
+    {name: 'Headphones', price: 50, quantity: 30, lowStockLevel: 7 }
+];
+
+// console.log(inventory)
+
